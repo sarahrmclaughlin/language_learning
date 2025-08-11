9996f762e18eccf2e355c019230fdd7d0944f762
sarahrmclaughlin <srmclaughlin41@gmail.com>
2025-08-11 08:31:21 -0400

    update notebook

--- Makefile
+++ Makefile
@@ -3,12 +3,13 @@ PIP = pip3
 
 install-dev:
 	$(PIP) install -r requirements.txt
-	$(PIP) install black isort flake8 pylint
+	$(PIP) install black isort flake8 pylint lint
 
 format:
 	black .
 	isort .
 lint:
 	flake8 src/ --max-line-length=88
-	flake8 eda_notebook.ipynb --max-line-length=88
-	pylint *.py
+	flake8 eda_notebook.ipynb --max-line-length=200
+# 	pylint *.py
+# 	pylint src/*.py
--- src/process_yaml_functions.py
+++ src/process_yaml_functions.py
@@ -1,5 +1,6 @@
-import yaml 
+import yaml
 from pathlib import Path
+
 
 def get_and_open_yaml_config(yaml_file_name: str) -> dict:
     """
@@ -10,9 +11,9 @@ def get_and_open_yaml_config(yaml_file_name: str) -> dict:
         dict: The contents of the YAML file as a dictionary.
     """
 
-    config_path = Path('configs')/f"{yaml_file_name}.yaml"
+    config_path = Path("configs") / f"{yaml_file_name}.yaml"
 
-    with open(config_path, 'r') as file:
+    with open(config_path, "r") as file:
         config = yaml.safe_load(file)
 
     return config
